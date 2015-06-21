c8c57447ae292ca7dd64c08bdaf1478d2988324f
Zach Walsh <ztwalsh@gmail.com>
2015-06-21 18:06:49 -0500

    Removing the powerreviews directory to use everything to sandbox subdomain.

--- powerreviews/ryp/lib/include.head.php
+++
@@ -1,7 +0,0 @@
-<title><?php echo $title; ?></title>
-<link href="css/styles.css" media="all" rel="stylesheet" type="text/css" />
-<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-<script src="js/jquery.js" type="text/javascript"></script>
-<script src="js/popup.js" type="text/javascript"></script>
-<script src="js/behavior.js" type="text/javascript"></script>
--- powerreviews/snippets/lib/include.header.php
+++
@@ -1,4 +0,0 @@
-<header>
-	<a href="index.php">Reco Score</a>
-	<a href="star-reco.php">Star/Reco Score</a>
-</header>
