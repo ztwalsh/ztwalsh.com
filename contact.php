fd9e16400c8ca12b47d150b64b40dc1256361c98
Zach Walsh <ztwalsh@gmail.com>
2017-05-09 23:25:46 -0500

    Orbitz page

---
+++ contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<?php
+		$page_title = '';
+		$page = 'home';
+		require('lib/include.head.php');
+	?>
+	<body>
+
+		<?php require('lib/include.nav.php'); ?>
+
+		<section class="content text-center">
+			<section>
+				<img class="zach" height="" src="images/zachwalsh.jpg" width="" />
+				<h1 class="heading-1">Get in Touch</h1>
+				<h2 class="heading-3 small">
+					<i class="fa fa-twitter" aria-hidden="true"></i> &nbsp;@ztwalsh<br />
+					<i class="fa fa-dribbble" aria-hidden="true"></i> &nbsp;@ztwalsh<br />
+					<i class="fa fa-envelope" aria-hidden="true"></i> &nbsp;ztwalsh@gmail.com
+				</h2>
+			</section>
+		</section>
+	</body>
+</html>
