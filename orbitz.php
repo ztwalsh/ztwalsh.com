b3554ff60d06459ce70a49db93a959ce6fee8eb4
Zach Walsh <ztwalsh@gmail.com>
2017-05-10 23:30:06 -0500

    Orbitz images.

--- orbitz.php
+++ orbitz.php
@@ -30,7 +30,7 @@
 
 		<section class="images">
 			<figure>
-				<img src="images/shakoolie_web-02.jpg" />
+				<img src="images/orbitz_web-02.jpg" />
 				<figcaption class="left">
 					<h3 class="heading-3">We Want to Clearly Tell Shoppers What They'll Get</h3>
 					The category page is set up to make seeing our range of koozie design easy.
@@ -38,13 +38,7 @@
 					The product page was built as a showroom for our koozies. Large photos of the koozie and its parts give buyers the visual information they need to add to cart.</figcaption>
 			</figure>
 			<figure>
-				<img src="images/shakoolie_web-01.jpg" />
-			</figure>
-			<figure>
-				<img src="images/shakoolie_web-03.jpg" />
-			</figure>
-			<figure>
-				<img src="images/shakoolie_web-04.jpg" />
+				<img src="images/orbitz_web-04.jpg" />
 			</figure>
 		</section>
 
@@ -57,44 +51,13 @@
 
 		<section class="images">
 			<figure>
-				<img src="images/shakoolie_product-01.jpg" />
+				<img src="images/orbitz_web-05.jpg" />
 				<figcaption class="right">
 					<h3 class="heading-3">We Try, Fail, and Try Again to Find What Sells</h3>
 					As a ux and product designer, iteration is a part of my job. It's been fun to take that principal and apply it to developing designs that resonate with real people.
 					<br /><br />
 					We have had many different designs over the years. We'll do short runs of new designs, see what sticks, and move on. It's been a great way to keep the idea train running while also keeping inventory bloat light.
 				</figcaption>
-			</figure>
-			<figure>
-				<img src="images/shakoolie_product-02.jpg" />
-			</figure>
-		</section>
-
-
-		<section class="content spacer">
-				<h3 class="heading-2">Shipping Design</h3>
-				<p>Lorem ipsum dolor sit amet, ullamcorper nec eget convallis, ultricies viverra id libero diam praesent in. Posuere euismod id vel laoreet non egestas, ipsum primis mi sollicitudin metus, diam ac, elit vivamus suscipit varius. Mus morbi placerat metus, risus turpis feugiat magna, ut lacinia semper in ea elit ut, nonummy etiam vehicula duis. Sagittis vivamus lectus viverra eleifend torquent vitae. Tellus id at, vitae provident sed mauris tellus. Eu duis maecenas, lorem tincidunt ut. Habitant mi eu. Feugiat suspendisse id lectus, integer lacinia aliquam. Luctus dolor eu risus dolorum elementum, tincidunt leo quis, per lorem, tortor maecenas vehicula vivamus. Nisl quis eros proin condimentum, dolor quis, eleifend velit aliquam praesent. Tempor interdum auctor arcu orci ac.</p>
-		</section>
-
-
-		<section class="images">
-			<figure>
-				<img src="images/shakoolie_shipping-01.jpg" />
-				<figcaption class="right">
-					<h3 class="heading-3">We Make Customers Excited to Get Their Order</h3>
-					Our shipping packaging started as anything but exciting. Simpe Tyvek envelopes, the stark white polymailers. As we've grown, though, we wanted to add excitement to the moment the customer sees that Shakoolie package on their front door.
-					<br /><br />
-					We chose to go long on our bold blue color and added the simple phrase "Take me to your Shower" to make the experience of opening feel special.
-				</figcaption>
-			</figure>
-			<figure>
-				<img src="images/shakoolie_shipping-02.jpg" />
-			</figure>
-			<figure>
-				<img src="images/shakoolie_shipping-04.jpg" />
-			</figure>
-			<figure>
-				<img src="images/shakoolie_shipping-05.jpg" />
 			</figure>
 		</section>
 	</body>
