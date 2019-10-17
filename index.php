9a2ed02f85ae8b911cd0aa12799cc7597aefdf5e
Zach Walsh <zachwalsh@ip-172-27-243-15.ec2.internal>
2019-10-16 23:19:33 -0500

    Adding some flex grids

--- index.php
+++ index.php
@@ -9,45 +9,20 @@
 
 		<?php require('lib/include.nav.php'); ?>
 
-		<section class="text-center">
-			<section class="image-title">
-				<img class="zach" height="" src="images/zachwalsh_02.jpg" width="" />
-				<h1 class="heading-1">Fuck Yeah I Zach Walsh</h1>
-				<h2 class="heading-3 small">UX + Product Design</h2>
-			</section>
+		<section class="grid-container">
+			<div>
+				Stuff
+			</div>
+			<div>
+				Stuff
+			</div>
 		</section>
-		<section class="content text-center">
-			<div class="wrapper">
-				<h2 class="heading-2">Project Highlights</h2>
-				<section class="tiles cf">
-					<div class="powerreviews.php">
-						<a href="powerreviews.php">
-							<img src="images/powerreviews_dashboard-01.jpg" width="100%" height="auto" />
-							<h4 class="heading-4">PowerReviews</h4>
-							<p class="small">Giving brands and retailers a connection to their customers.</p>
-						</a>
-					</div>
-					<div class="orbitz.php">
-						<a href="powerreviews.php">
-							<img src="images/orbitz_web-01.jpg" width="100%" height="auto" />
-							<h4 class="heading-4">Orbitz</h4>
-							<p class="small">Helping consumers shop for and book the flights that work for them.</p>
-						</a>
-					</div>
-					<!-- <div class="photowave.php">
-						<a href="photowave.php">
-							<h4 class="heading-4">Photowave</h4>
-							<p class="small">Helping teams visualize progress to themselves and their customers.</p>
-						</a>
-					</div> -->
-					<div class="shakoolie.com">
-						<a href="shakoolie.php">
-							<img src="images/shakoolie_web-06.jpg" width="100%" height="auto" />
-							<h4 class="heading-4">Shakoolie</h4>
-							<p class="small">Unlocking the pure joy of drinking a beer in the shower.</p>
-						</a>
-					</div>
-				</section>
+		<section class="grid-container">
+			<div>
+				Stuff
+			</div>
+			<div>
+				Stuff
 			</div>
 		</section>
 	</body>
